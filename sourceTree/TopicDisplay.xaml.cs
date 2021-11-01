94d8cfe6be913bece3363ec43f099be30f2147ad
Maxim Usik <42520857+musik8@users.noreply.github.com>
2021-11-01 18:40:34 -0400

    Add files via upload

---
+++ sourceTree/TopicDisplay.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+
+namespace sourceTree
+{
+   
+    public partial class TopicDisplay : ContentPage
+    {
+
+      
+        PhotoManager pm = new PhotoManager();
+         DatabaseManager db;
+        Topic currTopic;
+        ObservableCollection<Topic> subTopics;
+
+        private string _Title;
+        public string Title
+        {
+            get { return _Title; }
+            set { _Title = value; OnPropertyChanged(); }
+        }
+
+
+        public TopicDisplay(Topic mTopic, DatabaseManager db)
+        {
+
+
+            this.currTopic = mTopic;
+            this.db = db;
+            this.BindingContext = this;
+           
+            InitializeComponent();
+        }
+        protected async override void OnAppearing()
+        {
+           
+            Title = this.currTopic.Title;
+            onlineUrl.Text = this.currTopic.OnlineImagePath;
+            imageDisplay.Source = this.currTopic.ImagePath;
+            TitleText.Text = this.currTopic.Title;
+            SourceText.Text = this.currTopic.Source;
+            DescriptionText.Text = this.currTopic.Desc;
+
+            currTopic = await db.getTopic(this.currTopic.Id);
+            subTopics = new ObservableCollection<Topic>(currTopic.subTopics);
+            SubTopicTable.ItemsSource = subTopics;
+            base.OnAppearing();
+
+        }
+
+        private void addSubTopic(object sender, EventArgs e)
+        {
+
+        }
+     
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+
+            (sender as Button).IsEnabled = false;
+
+            var updatedTopic = await pm.UploadImage(currTopic);
+            onlineUrl.Text = updatedTopic;
+            currTopic.OnlineImagePath = updatedTopic;
+            db.updateTopic(currTopic);
+            (sender as Button).IsEnabled = true;
+        }
+
+        private async void  SubTopicTable_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            await this.Navigation.PushAsync(new TopicDisplay(SubTopicTable.SelectedItem as Topic, this.db));
+        }
+        //public void RefreshData()
+        //{
+        //    listItems.Add("Xamarin Monkeys");
+        //    SubTopicTable.ItemsSource = null;
+        //    SubTopicTable.ItemsSource
+        //    listPlatforms.ItemsSource = listItems;
+        //    currTopic = await db.getTopic(this.currTopic.Id);
+        //    subTopics = new ObservableCollection<Topic>(currTopic.subTopics)
+        //}
+        private async void Add_Clicked(object sender, EventArgs e)
+        {
+            Topic newTopic = await TopicEditor.InputBox(this.Navigation, null);
+            db.InsertSubTopic(newTopic, currTopic);
+          
+        }
+
+        private async void Edit_Clicked(object sender, EventArgs e)
+        {
+            Topic newTopic = await TopicEditor.InputBox(this.Navigation, currTopic);
+            db.updateTopic(newTopic);
+        }
+
+        private async void Share_Clicked(object sender, EventArgs e)
+        {
+
+            string ClipBoard = "";
+            string UrlLink;
+            if (currTopic.OnlineImagePath == "")
+            {
+                UrlLink = await pm.UploadImage(currTopic);
+                currTopic.OnlineImagePath = UrlLink;
+                onlineUrl.Text = UrlLink;
+                db.updateTopic(currTopic);
+
+            }
+
+            ClipBoard = currTopic.Title + "\n" + currTopic.OnlineImagePath + "\n" + "Source: " + currTopic.Source + "\n" + currTopic.Desc;
+
+            await Clipboard.SetTextAsync(ClipBoard);
+        }
+
+
+
+    }
+}
---
+++ sourceTree/TopicEditor.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using System.IO;
+
+namespace sourceTree
+{
+    public class TopicEditor
+    {
+
+        
+        public ObservableCollection<Topic> getTopic()
+        {
+            var tasks = new ObservableCollection<Topic>();
+            return tasks;
+        }
+       
+        public static Task<Topic> InputBox(INavigation navigation, Topic editTopic, bool rootTopic = true)
+        {
+
+           
+            if (editTopic == null)
+            {
+                editTopic = new Topic();
+                editTopic.isSub = rootTopic;    
+            }
+
+                
+           
+            var tcs = new TaskCompletionSource<Topic>();
+
+            var lblTitle = new Label { Text = "Title:", HorizontalOptions = LayoutOptions.Center, FontAttributes = FontAttributes.Bold, TextType = TextType.Html };
+            var lblMessage = new Label { Text = "Add A new Topic:" ,FontAttributes = FontAttributes.Bold };
+            var Sinput = new Label { Text = "Enter Source:", FontAttributes = FontAttributes.Bold  };
+            var Dinput = new Label { Text = "Enter Description:" , FontAttributes = FontAttributes.Bold };
+            var Imginput = new Label { Text = "Image Path" , FontAttributes = FontAttributes.Bold };
+            var TitleInput = new CustomEntry { 
+                Text = editTopic.Title,
+                HorizontalOptions = LayoutOptions.Fill,
+              
+              
+            };
+            var SourceInput = new Entry { Text = editTopic.Source };
+            var DescInput = new Editor { Text = editTopic.Desc,
+                HeightRequest = 200,  
+            };
+            var imagePath = editTopic.ImagePath;
+            var onlineImageLink = editTopic.OnlineImagePath;
+
+
+            var btnAddImage = new Button
+            {
+                Text = "Pick Image",
+                BackgroundColor = Color.FromRgb(133, 219, 197),
+            };
+
+            btnAddImage.Clicked += async (s, e) =>
+            {
+                (s as Button).IsEnabled = false;
+                PhotoManager pm = new PhotoManager();
+                imagePath = await pm.AddImageAsync();
+                Imginput.Text = imagePath;
+                onlineImageLink = "";
+                (s as Button).IsEnabled = true;
+            };
+
+            var btnOk = new Button
+            {
+                Text = "Add",
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                BackgroundColor = Color.FromRgb(255, 143, 97),
+            };
+            btnOk.Clicked += async (s, e) => 
+            {
+               
+                editTopic.Title = TitleInput.Text;
+                editTopic.Source = SourceInput.Text;
+                editTopic.Desc = DescInput.Text;
+                editTopic.ImagePath = imagePath;
+                editTopic.OnlineImagePath = onlineImageLink;
+                await navigation.PopModalAsync();
+                tcs.SetResult(editTopic);
+            };
+
+            var btnCancel = new Button
+            {
+                Text = "Cancel",
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                BackgroundColor = Color.FromRgb(255, 143, 97)
+            };
+            btnCancel.Clicked += async (s, e) =>
+            {
+              
+                await navigation.PopModalAsync();
+                tcs.SetResult(null);
+            };
+
+            var slButtons = new StackLayout
+            {
+                Orientation = StackOrientation.Horizontal,
+                HorizontalOptions = LayoutOptions.Fill,
+
+                Children = { btnOk, btnCancel },
+            };
+            var isUrgentLayout = new StackLayout
+            {
+                Orientation = StackOrientation.Horizontal,
+                Children = { btnAddImage },
+
+            };
+
+            var layout = new StackLayout
+            {
+                // Padding = new Thickness(0, 40, 0, 0),
+                VerticalOptions = LayoutOptions.Start,
+                HorizontalOptions = LayoutOptions.Fill,
+                Orientation = StackOrientation.Vertical,
+                Margin = 10,
+                Children = { lblTitle, lblMessage, TitleInput, Sinput, SourceInput, Dinput, DescInput, Imginput, btnAddImage, slButtons },
+            };
+
+          
+            var page = new ContentPage();
+            page.Content = layout;
+            navigation.PushModalAsync(page);
+          
+            return tcs.Task;
+        }
+    }
+}
